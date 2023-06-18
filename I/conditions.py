ed6b42e12818828fe26af543b71dc729a30f6c10
Hamza Plojovic <hamzaplojovic9@gmail.com>
2023-06-18 11:54:56 +0200

    about me website task

--- I/conditions.py
+++ I/conditions.py
@@ -1,6 +1,23 @@
-a = 4
+# a = 10
+# if a > 5: # Preskace se ako je False
+#     print("A je vece od 5") # Ispisace se ako je True
+# print(100+200)
 
-if a > 5 or a > 7:
-    print("Uslov ispunjen")
+# NOTE: if u pythonu oznacava uslov
+#       sastoji se iz reci if koja oznaca uslov
+#       iz boolean operacije koja vraca True ili False
+#       iz koda koji ce da se izvrsi ako je uslov True
+#       ako je uslov false, nastavljamo izvan if grane
 
-print("Uslov nije ispunjen")
+# a = 10
+# if a > 5:
+#     rezultat = "A je vece od 5"
+#     print(rezultat)
+
+# print(1+1)
+
+# NOTE: ZADATAK
+# KORISTITI int(input("Unesi prvi broj: ")) i
+# int(input("Unesi drugi broj: ")) i pomnoziti ih
+# i sa if proveriti je li proizvod veci od 100
+# ako je veci od 100, ispisi proizvod je veci od 100
--- II/sets.py
+++ II/sets.py
@@ -12,13 +12,35 @@
 
 # ==================================================
 
-# moj_set = {1, 2, 3}
+# SET METHODS
 
-# for x in moj_set:
-#     print(x)
+# add
+# remove
+# clear
+# discard
+# pop
+# update
+# union
+# difference and intersection
 
-# print(10 in moj_set)
+moj_set = {"apple", "banana", "cherry", "mango"}
+moj_drugi_set = {"papaya", "grapes", "orange", "watermelon"}
+moj_treci_set = {"bmw", "audi", "mercedes"}
+moj_cetvrti_set = {"fiat", "audi", "bmw"}
 
-broj = 2
-print(broj)
-del broj
+
+# moj_set.add("ananas")   # Added ananas in set
+# moj_set.remove("kiwi")  # Tried to remove, but error
+# moj_set.discard("kiwi") # Tried to remove, but went past it
+# moj_set.clear()         # void function, ne prima vrednost,  brise sve vrednosti
+# moj_set.pop()           # Removes first element after initialization
+
+# moj_set.update(moj_drugi_set)  # Added a set on the set the method is called on
+# moj_set.union(moj_drugi_set)  # returns the value
+
+razlika = moj_treci_set.difference(moj_cetvrti_set)
+presek = moj_treci_set.intersection(moj_cetvrti_set)
+
+# print(moj_set)
+print(razlika)
+print(presek)
