264e77bf43dcc8493b6904932e6d522472326093
Hamza Plojovic <hamzaplojovic9@gmail.com>
2023-09-24 11:50:16 +0200

    update

---
+++ II-2/dictionary/dictionary.py
@@ -0,0 +1,5 @@
+moj_dictionary = {"car": "Rollce Royce", "school": "Medresa Gazi Isa beg"}
+
+moj_dictionary["age"] = 100
+
+print(moj_dictionary)
---
+++ II-2/sets/set_methods.py
@@ -0,0 +1,19 @@
+moj_set = {1, 2, 3, 4, 5, 6, 7}
+drugi_set = {1, 2, 10, 15, 14}
+treci_set = {1, 10, 15, 18}
+
+# ADD - prima element - dodaje element u set
+# POP - ne prima argument - izbacuje prvi element iz seta
+# CLEAR - ne prima argument - izbrise sve elemente seta
+# INTERSECTION - prima argumente - nalazi elemente koji se ponavljaju u svakom od setova poslatih kao argument
+# UNION - prima argumente - spaja sve elemente setova poslatih kao argument sa setom sa kog je pozvana
+
+# moj_set.add(8)
+# moj_set.pop()
+# moj_set.clear()
+# print(moj_set.intersection(drugi_set, treci_set))
+# print(
+#     moj_set.union(
+#         drugi_set, treci_set, [1, 2, 3, 3, 4, 5, 6, 6, 19], (100, 1000, 10000, 100000)
+#     )
+# )
